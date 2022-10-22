80afb02d20a6ca3324d6e6edc9da2f919863069d
Simon <42054370+coeus-lei@users.noreply.github.com>
2022-10-22 18:12:01 +0800

    Update expor_excel.sql

--- mysql_operates/expor_excel.sql
+++ mysql_operates/expor_excel.sql
@@ -1,5 +1,24 @@
-#This is a sample example
+#This is a sample example for xls formart
 select * into outfile '/var/lib/mysql-files/outfile.xls' 
+from (
+select 
+CONVERT(('编号') USING gbk),
+CONVERT(('名字') USING gbk),
+CONVERT(('网址') USING gbk),
+CONVERT(('数值') USING gbk),
+CONVERT(("国家") USING gbk) #写表头并且修改为gbk格式，支持中文
+UNION 
+select 
+id,
+CONVERT((name) USING gbk),
+CONVERT((url) USING gbk),
+CONVERT((alexa) USING gbk),
+CONVERT((country) USING gbk),
+from Websites) b;#查询的数据
+##############################################
+#This is a sample example for csv formart
+select * into outfile '/var/lib/mysql-files/outfile.csv' 
+fields terminated by ','  #设定分隔符，excel表格中自动匹配
 from (
 select 
 CONVERT(('编号') USING gbk),
