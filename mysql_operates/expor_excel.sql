c14823d5055b70e9de80c0b9fe46885a7705ee0a
Simon <42054370+coeus-lei@users.noreply.github.com>
2022-10-22 18:05:26 +0800

    Create expor_excel.sql

---
+++ mysql_operates/expor_excel.sql
@@ -0,0 +1,17 @@
+#This is a sample example
+select * into outfile '/var/lib/mysql-files/outfile.xls' 
+from (
+select 
+CONVERT(('编号') USING gbk),
+CONVERT(('名字') USING gbk),
+CONVERT(('网址') USING gbk),
+CONVERT(('数值') USING gbk),
+CONVERT(("国家") USING gbk) #写表头并且修改为gbk格式，支持中文
+UNION 
+select 
+id,
+CONVERT((name) USING gbk),
+CONVERT((url) USING gbk),
+CONVERT((alexa) USING gbk),
+CONVERT((country) USING gbk),
+from Websites) b;#查询的数据
